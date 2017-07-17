aa8b1b0a3ea164b8d672aad064a07afeaf913700
Deepak Kafle <deepak.kafle@librtymutual.com>
2017-07-17 11:02:57 -0400

    mock data is working

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
-import './index.css'
-import numeral from 'numeral'
+import {getUsers} from './api/userApi'
+import {deleteUser} from './api/userApi'
 
-/* eslint-disable no-console */
+getUsers().then(result => {
+  let usersBody = ''
 
-const courseValue = numeral(1000).format('$0,0.00')
-console.log(`I'd like you to pay ${courseValue}`)
+  result.forEach(user => {
+    usersBody += `<tr>
+      <td><a href="#" data-id="${user.id}" class="delete-user">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+    </tr>`
+  })
+
+  global.document.getElementById('users').innerHTML = usersBody
+
+  const deleteLinks = global.document.getElementsByClassName('delete-user')
+  console.log(deleteLinks)
+
+  Array.from(deleteLinks, link => {
+    link.onclick = function (evt) {
+      console.log(evt)
+      const el = evt.target
+      evt.preventDefault()
+      deleteUser(el.attributes['data-id'].value)
+      const row = el.parentNode.parentNode
+      row.parentNode.removeChild(row)
+    }
+  })
+})
